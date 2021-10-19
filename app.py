4a7699be0d6d817151303e25673aebce5a948ce3
Ramirors31 <ramirezsiqueiros@hotmail.com>
2021-10-18 19:17:18 -0500

    control 1

--- app.py
+++ app.py
@@ -8,10 +8,6 @@ class VentanaPrincipal(QMainWindow):
         super(VentanaPrincipal, self).__init__()
         loadUi('login.ui',self)
 
-class MenuPrincipal(QMainWindow):
-    def __init__(self,parent=None):
-        super(MenuPrincipal,self).__init__(parent)
-        loadUi('menuInicio.ui',self)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
